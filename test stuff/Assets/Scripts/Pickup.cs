910f8d976e8b75a69ae5363e62b8d5eff17e0dc7
mustard221 <lailar2305@gmail.com>
2025-12-15 12:08:30 +0000

    basic end scene & win trigger added

--- test stuff/Assets/Scripts/Pickup.cs
+++ test stuff/Assets/Scripts/Pickup.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class Pickup : MonoBehaviour
 {
     public UnityEvent enteredTrigger, exitedTrigger, interacted;
 
     public AudioSource s;
+    public int winAmount = 10; // number of items needed to win
 
     private bool insideTrigger;
     private static int itemCount = 0; // track collected items
@@ -22,6 +24,12 @@ public class Pickup : MonoBehaviour
             insideTrigger = false;
             itemCount++;
             MonsterSpawn2.updateSpeed(itemCount); // notify spawner
+        }
+
+        if (itemCount >= winAmount)
+        {
+            // trigger win condition
+            SceneManager.LoadScene("EndScene");
         }
     }
 
